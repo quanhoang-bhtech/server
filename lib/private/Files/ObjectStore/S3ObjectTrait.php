27bef6e5a3a6640ce10a880e5aed96e9394744ca
Julius Härtl <jus@bitgrid.net>
2022-06-09 20:56:18 +0200

    [S3] Add option to specify an SSE-C customer provided key
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/private/Files/ObjectStore/S3ObjectTrait.php
+++ lib/private/Files/ObjectStore/S3ObjectTrait.php
@@ -44,6 +44,7 @@ trait S3ObjectTrait {
 	abstract protected function getConnection();
 
 	abstract protected function getCertificateBundlePath(): ?string;
+	abstract protected function getSSECParameters(): array;
 
 	/**
 	 * @param string $urn the unified resource name used to identify the object
@@ -57,7 +58,7 @@ trait S3ObjectTrait {
 				'Bucket' => $this->bucket,
 				'Key' => $urn,
 				'Range' => 'bytes=' . $range,
-			]);
+			] + $this->getSSECParameters());
 			$request = \Aws\serialize($command);
 			$headers = [];
 			foreach ($request->getHeaders() as $key => $values) {
@@ -100,7 +101,7 @@ trait S3ObjectTrait {
 			'Body' => $stream,
 			'ACL' => 'private',
 			'ContentType' => $mimetype,
-		]);
+		] + $this->getSSECParameters());
 	}
 
 
@@ -119,7 +120,7 @@ trait S3ObjectTrait {
 			'part_size' => $this->uploadPartSize,
 			'params' => [
 				'ContentType' => $mimetype
-			],
+			] + $this->getSSECParameters(),
 		]);
 
 		try {
@@ -174,10 +175,10 @@ trait S3ObjectTrait {
 	}
 
 	public function objectExists($urn) {
-		return $this->getConnection()->doesObjectExist($this->bucket, $urn);
+		return $this->getConnection()->doesObjectExist($this->bucket, $urn, $this->getSSECParameters());
 	}
 
 	public function copyObject($from, $to) {
-		$this->getConnection()->copy($this->getBucket(), $from, $this->getBucket(), $to);
+		$this->getConnection()->copy($this->getBucket(), $from, $this->getBucket(), $to, 'private', $this->getSSECParameters());
 	}
 }
